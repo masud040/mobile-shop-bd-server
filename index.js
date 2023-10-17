a2fce2c5dc724cf7e97f6e89696c359ff70d749d
masudo40 <masudinfo128@gamil.com>
2023-10-17 22:51:26 +0600

    add mongodb database and create two api post product and get brand

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -7,7 +8,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ibjkumr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollections = client.db("ProductDB").collection("product");
+    const brandCollections = client.db("BrandDB").collection("brand");
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollections.insertOne(product);
+      res.send(result);
+    });
+    app.get("/brands", async (req, res) => {
+      const result = await brandCollections.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Technology and Electronics management server is running");
 });
